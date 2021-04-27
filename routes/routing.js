d42eaade9ec69bf3c8e97c2c7d23c70d2d24b11f
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-04-27 04:29:53 +0300

    add routes of genre pages

--- routes/routing.js
+++ routes/routing.js
@@ -67,5 +67,79 @@ router.get('/add', (req, res) => {
     })
 })
 
+//роутеры для страниц
+
+router.get('/genres/pop', (req, res) => {
+    res.render('pop', {
+        title: 'Слушайте и качайте музыкальные композиции популярных поп-исполнителей.'
+    })
+});
+
+router.get('/genres/rock', (req, res) => {
+    res.render('rock', {
+        title: 'Слушайте и качайте музыкальные композиции популярных рок-исполнителей.'
+    });
+});
+
+router.get('/genres/rap', (req, res) => {
+    res.render('rap', {
+        title: 'Слушайте и качайте музыкальные композиции популярных реп-исполнителей.'
+    });
+});
+
+router.get('/genres/electronics', (req, res) => {
+    res.render('electronics', {
+        title: 'Слушайте и качайте популярные музыкальные электроник композиции.'
+    });
+});
+
+router.get('/genres/chanson', (req, res) => {
+    res.render('chanson', {
+        title: 'Слушайте и качайте музыкальные композиции популярных шансонье.'
+    })
+});
+
+router.get('/genres/metal', (req, res) => {
+    res.render('metal', {
+        title: 'Слушайте и качайте музыкальные композиции популярных рок-металлистов.'
+    });
+});
+
+router.get('/genres/classic', (req, res) => {
+    res.render('classic', {
+        title: 'Слушайте и качайте музыкальные композиции популярных классиков.'
+    })
+});
+
+router.get('/genres/rnb', (req, res) => {
+    res.render('rnb', {
+        title: 'Слушайте и качайте музыкальные композиции популярных исполнителей rnb.'
+    })
+});
+
+router.get('/genres/jazz', (req, res) => {
+    res.render('jazz', {
+        title: 'Слушайте и качайте музыкальные композиции популярных jazz-исполнителей.'
+    });
+});
+
+router.get('/genres/country', (req, res) => {
+    res.render('country', {
+        title: 'Слушайте и качайте музыкальные композиции популярных исполнителей кантри.'
+    });
+});
+
+router.get('/genres/folk', (req, res) => {
+    res.render('folk', {
+        title: 'Слушайте и качайте музыкальные композиции популярных фолк-исполнителей.'
+    })
+});
+
+router.get('/genres/instrumental', (req, res) => {
+    res.render('instrumental', {
+        title: 'Слушайте и качайте популярную инструментальную музыку.'
+    })
+});
+
 //экспортируем во вне наш router
 module.exports = router;
