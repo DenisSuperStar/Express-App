129f16f3294f97bb5e1fbbf396f9816db9fd2efc
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-05-13 02:08:23 +0300

    added rendering pages with title of pages

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,71 +1,72 @@
 module.exports.pop = (req, res) => {
-    res.render({
+    res.render('pop', {
         title: 'Слушайте и качайте музыкальные композиции популярных поп-исполнителей.'
     });
 }
 
+
 module.exports.rock = (req, res) => {
-    res.render({
+    res.render('rock', {
         title: 'Слушайте и качайте музыкальные композиции популярных рок-исполнителей.'
     });
 }
 
 module.exports.rap = (req, res) => {
-    res.render({
+    res.render('rap', {
         title: 'Слушайте и качайте музыкальные композиции популярных реп-исполнителей.'
     });
 }
 
 module.exports.electronics = (req, res) => {
-    res.render({
+    res.render('electronics', {
         title: 'Слушайте и качайте популярные музыкальные электроник композиции.'
     });
 }
 
 module.exports.chanson = (req, res) => {
-    res.render({
+    res.render('chanson', {
         title: 'Слушайте и качайте музыкальные композиции популярных шансонье.'
     });
 }
 
 module.exports.metal = (req, res) => {
-    res.render({
+    res.render('metal', {
         title: 'Слушайте и качайте музыкальные композиции популярных рок-металлистов.'
     });
 }
 
 module.exports.classic = (req, res) => {
-    res.render({
+    res.render('classic', {
         title: 'Слушайте и качайте музыкальные композиции популярных классиков.'
     });
 }
 
 module.exports.rnb = (req, res) => {
-    res.render({
-        title: 'Слушайте и качайте музыкальные композиции популярных rnb исполнителей.'
+    res.render('rnb', {
+        title: 'Слушайте и качайте музыкальные композиции популярных rnb-исполнителей.'
     });
 }
 
 module.exports.jazz = (req, res) => {
-    res.render({
+    res.render('jazz', {
         title: 'Слушайте и качайте музыкальные композиции популярных jazz-исполнителей.'
     });
 }
 
 module.exports.country = (req, res) => {
-    res.render({
+    res.render('country', {
         title: 'Слушайте и качайте музыкальные композиции популярных исполнителей кантри.'
     });
 }
 
 module.exports.folk = (req, res) => {
-    res.render({
+    res.render('folk', {
         title: 'Слушайте и качайте музыкальные композиции популярных фолк-исполнителей.'
     });
 }
 
 module.exports.instrumental = (req, res) => {
-    res.render({
-        title: 'Слушайте и качайте популярную инструментальную музыку.'
+    res.render('instrumental', {
+        title: 'Слушайте и качайте музыкальные композиции популярных фолк-исполнителей.'
     });
 }
