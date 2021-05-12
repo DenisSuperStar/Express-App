42aaf4b8474f1b4ce392ef58ec3d1582d1c51234
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-05-12 22:27:41 +0300

    created genres controller

---
+++ controllers/genreController.js
@@ -0,0 +1,71 @@
+module.exports.pop = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных поп-исполнителей.'
+    });
+}
+
+module.exports.rock = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных рок-исполнителей.'
+    });
+}
+
+module.exports.rap = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных реп-исполнителей.'
+    });
+}
+
+module.exports.electronics = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте популярные музыкальные электроник композиции.'
+    });
+}
+
+module.exports.chanson = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных шансонье.'
+    });
+}
+
+module.exports.metal = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных рок-металлистов.'
+    });
+}
+
+module.exports.classic = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных классиков.'
+    });
+}
+
+module.exports.rnb = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных rnb исполнителей.'
+    });
+}
+
+module.exports.jazz = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных jazz-исполнителей.'
+    });
+}
+
+module.exports.country = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных исполнителей кантри.'
+    });
+}
+
+module.exports.folk = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте музыкальные композиции популярных фолк-исполнителей.'
+    });
+}
+
+module.exports.instrumental = (req, res) => {
+    res.render({
+        title: 'Слушайте и качайте популярную инструментальную музыку.'
+    });
+}
