b638c5d2ad5aced31092a912ee952dd9568fcc60
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-05-21 00:18:16 +0300

    constructor user object supplemented some props

--- controllers/navController.js
+++ controllers/navController.js
@@ -1,5 +1,7 @@
 //подключение модуля работы с http errors
 const createError = require('http-errors');
+//подключение модуля генерации id
+const { v4: uuidv4 } = require('uuid');
 //подключение модуля генерации hash пароля
 const bcrypt = require('bcrypt');
 //подключение модуля passport
@@ -98,7 +100,7 @@ module.exports.add = async (req, res, next) => {
     }
 
     try {
-        const {personEmail, personPassword} = req.body;
+        const {personEmail, personPassword, ...userData} = req.body;
         const findExistEmail = await User.findOne({userEmail: personEmail});
 
         if (findExistEmail) {
@@ -108,7 +110,15 @@ module.exports.add = async (req, res, next) => {
         }
 
         const hashedPassword = bcrypt.hash(personPassword, saltLength);
-        const user = new User(personEmail, hashedPassword);
+        const user = new User(
+            uuidv4(),
+            userData.personFirstName,
+            userData.personLastName,
+            userData.personAge,
+            userData.personGender,
+            personEmail,
+            hashedPassword
+        );
 
         await user.save();
         res.send('Пользователь зарегистрирован.');
