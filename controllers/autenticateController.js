70d015b5ef90cd9763d175c79cc76bba402316d9
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-05-22 15:38:28 +0300

    added new autenticate controller

---
+++ controllers/autenticateController.js
@@ -0,0 +1,24 @@
+//подключение модуля passport-local
+const passportLocal = require('passport-local');
+//импорт интересующей стратегии
+const {Strategy} = passportLocal;
+//подключение файла модели пользователя
+const User = require('../models/user.js');
+
+//импорт функции, авторизации пользователя по email и пароль
+module.exports.checkDataUser = async (authenticate) => {
+    authenticate.use(new Strategy({
+        usernameField: 'authEmail',
+        passwordField: 'authPassword'
+    }, (username, password, done) => {
+        await User.findOne({userEmail: username}, (err, user) => {
+            if (err) return done(err);
+
+            if ((!user) || (!user.validPassword(password))) {
+                return done(null, false, {message: 'Некорректный email или пароль.'});
+            }
+
+            return done(null, user);
+        });
+    }));
+}
